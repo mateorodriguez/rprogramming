d1b2067bd1b7e16483a4595f16c4ac31b2095ba6
Juan Mateo Rodriguez Sanchez <mateorodriguez@outlook.com>
2017-01-06 00:27:00 -0500

    Preliminar dev of corr script

--- corr.R
+++ corr.R
@@ -3,21 +3,30 @@ corr <- function(directory, threshold = 0){
   tryCatch(
     
     {
-      dirPath <- paste(getwd(), "/", directory, sep="")
+      dirPath <- paste("./", directory, sep="")
       isValidDirectory(dirPath)
       
       files <- list.files(path=dirPath, pattern = "*.csv", full.names = TRUE)
       
+      csv_sulf <- data.frame(sulfate=numeric(0))
+      csv_nit <- data.frame(nitrate=numeric(0))
       
-      
-      
-      
-      
+      for(i in 1:length(files)){
+        
+        csv <- read.csv(file = files[i], header = TRUE, sep=",", dec=".")
+        if(nrow(csv[complete.cases(csv),]) > threshold){
+          
+          csv_sulf <- rbind(csv_sulf, csv[,c(2)])
+          csv_nit <- rbind(csv_nit, csv[,c(3)])
+        }
+        
+      }
+      cor(csv_sulf, csv_nit)
+    },
+    error = function(e)
+    {
+      print(e)
     }
-    
-    
-    
-    
   )
   
   
