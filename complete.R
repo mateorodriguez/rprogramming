337e1a10594c4140a14adaccd425d9abc170ee86
mateorodriguez <mateorodriguez@outlook.com>
2017-01-12 14:22:46 -0500

    Finish programming assignment 1 scripts: Air Pollution. The code was improved for better performance and readability.

--- complete.R
+++ complete.R
@@ -1,23 +1,34 @@
 complete <- function(directory, id = 1:332){
   
-  tryCatch(
+  # Create an empty numeric vector
+  nonaNum <- numeric(0)
+  
+  for(mid in id){
     
+    # Get the data frame of the given monitor ID
+    mDF <- getDF(mid, directory)
+    
+    # Count the number of complete cases and append it to vector
+    nonaNum <- c(nonaNum, nrow(na.omit(mDF)))
+  }
+  
+  # Return the data frame
+  data.frame(id = id, nobs = nonaNum)
+}
+
+
+getDF <- function(mid, directory){
+  
+  tryCatch(
     {
-      dirPath <- paste("./", directory, sep="")
-      isValidDirectory(dirPath)
-      summ <- data.frame(id=numeric(), nobs=numeric())
+      # Validate file path
+      isValidDirectory(paste("./", directory, sep=""))
       
-      for(i in 1:length(id)){
-        
-        filePath <- paste(dirPath, "/", formatC(id[i], width = 3, flag = 0), ".csv", sep="")
-        df <- read.csv(file = filePath, header = TRUE, sep=",", dec=".")
-        
-        newRow <- c(id[i], nrow(df[complete.cases(df),]))
-        
-        summ[nrow(summ)+1, ] <- newRow
-      }
-      summ
+      # Build the file path
+      filePath <- paste("./", directory, "/", formatC(mid, width = 3, flag = 0), ".csv", sep="")
       
+      # Return the data frame
+      mDF <- read.csv(filePath)
     },
     error = function(e)
     {
@@ -26,9 +37,39 @@ complete <- function(directory, id = 1:332){
   )
 }
 
+
 isValidDirectory <- function(directory){
   
   if(!dir.exists(directory))
     stop(call. = FALSE, "Specified directory does not exist.")
-  
 }
+
+
+# complete_last <- function(directory, id = 1:332){
+#   
+#   tryCatch(
+#     
+#     {
+#       dirPath <- paste("./", directory, sep="")
+#       isValidDirectory(dirPath)
+#       summ <- data.frame(id=numeric(), nobs=numeric())
+#       
+#       for(i in 1:length(id)){
+#         
+#         filePath <- paste(dirPath, "/", formatC(id[i], width = 3, flag = 0), ".csv", sep="")
+#         df <- read.csv(file = filePath, header = TRUE, sep=",", dec=".")
+#         
+#         newRow <- c(id[i], nrow(df[complete.cases(df),]))
+#         
+#         summ[nrow(summ)+1, ] <- newRow
+#       }
+#       summ
+#       
+#     },
+#     error = function(e)
+#     {
+#       print(e)
+#     }
+#   )
+# }
+
--- pollutantmean.R
+++ pollutantmean.R
@@ -1,46 +1,75 @@
 pollutantmean <- function(directory=getwd(), pollutant, id = 1:332) {
-
+  
   tryCatch(
-
     {
-      dirPath <- paste(getwd(), "/", directory, sep="")
-
-      isValidDirectory(dirPath)
-      isValidPollutant(pollutant)
-
-      pMean <- vector(mode='numeric')
-
-
-      for(i in 1:length(id)){
-
-        filePath <- paste(dirPath, "/", formatC(id[i], width=3, flag=0), ".csv", sep="")
-        newf <- read.csv(file = filePath, header = TRUE, sep = ",", dec=".");
-
-        pMean <- c(pMean, newf[,pollutant])
+      # Create en empty numeric vector
+      pMean <- numeric(0)
+      
+      for(mid in id){
+        
+        # Get data frame by the given monitor ID
+        mDF <- getDF(mid, directory)
+        
+        # Append the pollutant values without NAs
+        pMean <- c(pMean, na.omit(mDF[,pollutant]))
       }
-
-      mean(pMean, na.rm = TRUE);
+      # Calulate mean
+      mean(pMean)
     },
     error = function(e)
     {
       print(e)
     }
-
+    
   )
-
+  
 }
 
 isValidDirectory <- function(directory){
-
+  
   if(!dir.exists(directory))
     stop(call. = FALSE, "Specified directory does not exist.")
-
+  
 }
 
 
 isValidPollutant <- function(pollutant){
-
+  
   if(pollutant != "sulfate" & pollutant != "nitrate")
     stop(call. = FALSE, "Please enter a valid pollutant. Availables ones are 'sulfate' or 'nitrate'.")
-
+  
 }
+
+
+# pollutantmean <- function(directory=getwd(), pollutant, id = 1:332) {
+# 
+#   tryCatch(
+# 
+#     {
+#       dirPath <- paste(getwd(), "/", directory, sep="")
+# 
+#       isValidDirectory(dirPath)
+#       isValidPollutant(pollutant)
+# 
+#       pMean <- vector(mode='numeric')
+# 
+# 
+#       for(i in 1:length(id)){
+# 
+#         filePath <- paste(dirPath, "/", formatC(id[i], width=3, flag=0), ".csv", sep="")
+#         newf <- read.csv(file = filePath, header = TRUE, sep = ",", dec=".");
+# 
+#         pMean <- c(pMean, newf[,pollutant])
+#       }
+# 
+#       mean(pMean, na.rm = TRUE);
+#     },
+#     error = function(e)
+#     {
+#       print(e)
+#     }
+# 
+#   )
+# 
+# }
+
